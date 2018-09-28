1dcaced9ecd842327278cc1b74b7008d76527d6c
Antonio Mallia <me@antoniomallia.it>
2018-09-28 02:41:06 -0700

    Removed build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/amallia/Development/bit_vector
-# Build directory: /Users/amallia/Development/bit_vector/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external")
-subdirs("test")
--- build/external/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/amallia/Development/bit_vector/external/googletest/googletest
-# Build directory: /Users/amallia/Development/bit_vector/build/external/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
